c566c85b73a09f6b826490f5d5d09152acf6c5a3
MustafaArif519 <mustafaarif519@gmail.com>
2023-12-02 20:38:13 -0500

    Fixed reload bug

--- frontend/src/filesystem/Files.jsx
+++ frontend/src/filesystem/Files.jsx
@@ -5,12 +5,12 @@ import Dir from "./Dir"
 import FileUpload from './FileUpload';
 import DirUpload from './DirUpload';
 import DeleteModal from './DeleteModal';
-import { FilePlus, FolderPlus, ArrowLeftCircle, Trash2, Folder, FileText  } from '@geist-ui/icons'
+import { FilePlus, FolderPlus, ArrowLeftCircle, Trash2, Folder, FileText } from '@geist-ui/icons'
 
 
 
 
-const Files = ({user, token}) => {
+const Files = ({ user, token }) => {
   const [path, setPath] = useState('root');
   const [dirs, setDirs] = useState([]);
   const [files, setFiles] = useState([]);
@@ -19,7 +19,7 @@ const Files = ({user, token}) => {
   const [deleteFiles, setDeleteFiles] = useState([]);
 
   const apiUrl = 'http://localhost:8000/api/v1/files';
-  
+
   const [view, setView] = useState(false);
   const [type, setType] = useState(null);
 
@@ -39,158 +39,165 @@ const Files = ({user, token}) => {
 
 
 
-const handleButtonClick = () => {
-  setPath((prevPath) => {
-    // Extract the portion of the path before the last slash
-    const newPath = prevPath.substring(0, prevPath.lastIndexOf('/'));
-    return newPath;
-  });
-};
-
-const handleDeleteMode = () => {
-  setDeleteMode(!deleteMode);
-};
-
-const handleDeleteView = (id, name) => {
-  setViewDelete(!viewDelete);
-  setDeleteId(id);
-  setDeleteName(name);
-  console.log(name);
-};
-
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await fetch(apiUrl);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-
-      let userFilteredFiles;
-      let userFilteredDirs;
-
-      if (searchInput == "") {
-        // If search input is blank, display everything
-        userFilteredFiles = data.filter(
-          (item) => item.is_file && item.author === user && item.directory === path
-        );
-        userFilteredDirs = data.filter(
-          (item) => !item.is_file && item.author === user && item.directory === path
-        );
-      } else {
-        // Filter based on search input
-        userFilteredFiles = data.filter(
-          (item) =>
-            item.is_file &&
-            item.author === user &&
-            item.directory === path &&
-            item.title.toLowerCase().includes(searchInput.toLowerCase())
-        );
-
-        userFilteredDirs = data.filter(
-          (item) =>
-            !item.is_file &&
-            item.author === user &&
-            item.directory === path &&
-            item.title.toLowerCase().includes(searchInput.toLowerCase())
-        );
-      }
-
-      setFiles(userFilteredFiles);
-      setDirs(userFilteredDirs);
-
-      setDeleteDirs(userFilteredDirs);
-      setDeleteFiles(userFilteredFiles);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+  const handleButtonClick = () => {
+    setPath((prevPath) => {
+      // Extract the portion of the path before the last slash
+      const newPath = prevPath.substring(0, prevPath.lastIndexOf('/'));
+      return newPath;
+    });
   };
 
-  fetchData();
-}, [user, path, view, viewDelete, searchInput]);
+  const handleDeleteMode = () => {
+    setDeleteMode(!deleteMode);
+  };
+
+  const handleDeleteView = (id, name) => {
+    setViewDelete(!viewDelete);
+    setDeleteId(id);
+    setDeleteName(name);
+    console.log(name);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+
+        let userFilteredFiles;
+        let userFilteredDirs;
+
+        if (searchInput == "") {
+          console.log("running empty case");
+          // If search input is blank, display everything
+          console.log(user);
+          console.log(path);
+          userFilteredFiles = data.filter(
+            (item) => item.is_file && item.author == user && item.directory == path
+          );
+          console.log(data);
+          console.log(userFilteredFiles);
+          userFilteredDirs = data.filter(
+            (item) => !item.is_file && item.author == user && item.directory == path
+          );
+        } else {
+          // Filter based on search input
+          userFilteredFiles = data.filter(
+            (item) =>
+              item.is_file &&
+              item.author == user &&
+              item.directory == path &&
+              item.title.toLowerCase().includes(searchInput.toLowerCase())
+          );
+
+          userFilteredDirs = data.filter(
+            (item) =>
+              !item.is_file &&
+              item.author == user &&
+              item.directory == path &&
+              item.title.toLowerCase().includes(searchInput.toLowerCase())
+          );
+        }
+
+        setFiles(userFilteredFiles);
+        setDirs(userFilteredDirs);
+
+        setDeleteDirs(userFilteredDirs);
+        setDeleteFiles(userFilteredFiles);
+
+        console.log(userFilteredFiles);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [user, path, view, viewDelete, searchInput]);
 
 
 
   return (
-<>
-  <Grid.Container justify="center" alignItems="center">
+    <>
+      <Grid.Container justify="center" alignItems="center">
 
 
-  { type === "file" && <FileUpload setView={setView} 
-                        viewHandler = {viewHandler} view = {view} 
-                        token={token} path={path} user={user}/>}
+        {type === "file" && <FileUpload setView={setView}
+          viewHandler={viewHandler} view={view}
+          token={token} path={path} user={user} />}
 
-{ type === "dir" && <DirUpload setView={setView} 
-    viewHandler = {viewHandler} view = {view} 
-    token={token} path={path} user={user}/>}
-  {viewDelete && 
-  <DeleteModal user={user} token = {token} path = {path}
-    deleteMode={deleteMode} viewDelete = {viewDelete} setViewDelete={setViewDelete}
-    id = {deleteId} name = {deleteName}
-   />
-  }
-  
-
-    <Text>{path}</Text>
-  </Grid.Container>
+        {type === "dir" && <DirUpload setView={setView}
+          viewHandler={viewHandler} view={view}
+          token={token} path={path} user={user} />}
+        {viewDelete &&
+          <DeleteModal user={user} token={token} path={path}
+            deleteMode={deleteMode} viewDelete={viewDelete} setViewDelete={setViewDelete}
+            id={deleteId} name={deleteName}
+          />
+        }
 
 
-  
-  <Grid.Container justify="center">
-  {path !== 'root' && (
-      <Grid xs={24} md={8} lg={4}>
-        {/* Adjust the width as needed */}
-        <Button type="warning" iconRight={<ArrowLeftCircle />} auto onClick={handleButtonClick} />
-      </Grid>
-    )}
-    <Spacer w={2} />
-    <Button type="success" onClick={()=>viewHandler(true, "file")} iconRight={<FilePlus />} auto />
-    <Spacer w={2} />
-    <Button type="success"  onClick={()=>viewHandler(true, "dir")} iconRight={<FolderPlus />} auto />
-    <Spacer w={2} />
-{!deleteMode ?   <Button type="error"  onClick={handleDeleteMode} iconRight={<Trash2  />} auto />
-:
-    <Button type="warning"  onClick={handleDeleteMode} iconRight={<Trash2  />} auto />}
-  </Grid.Container>
-  <Spacer h={2} />
-    <Input label="Search" placeholder="Ex: furniture"
-     onChange={(e) => setSearchInput(e.target.value)}
+        <Text>{path}</Text>
+      </Grid.Container>
+
+
+
+      <Grid.Container justify="center">
+        {path !== 'root' && (
+          <Grid xs={24} md={8} lg={4}>
+            {/* Adjust the width as needed */}
+            <Button type="warning" iconRight={<ArrowLeftCircle />} auto onClick={handleButtonClick} />
+          </Grid>
+        )}
+        <Spacer w={2} />
+        <Button type="success" onClick={() => viewHandler(true, "file")} iconRight={<FilePlus />} auto />
+        <Spacer w={2} />
+        <Button type="success" onClick={() => viewHandler(true, "dir")} iconRight={<FolderPlus />} auto />
+        <Spacer w={2} />
+        {!deleteMode ? <Button type="error" onClick={handleDeleteMode} iconRight={<Trash2 />} auto />
+          :
+          <Button type="warning" onClick={handleDeleteMode} iconRight={<Trash2 />} auto />}
+      </Grid.Container>
+      <Spacer h={2} />
+      <Input label="Search" placeholder="Ex: furniture"
+        onChange={(e) => setSearchInput(e.target.value)}
       />
-  <Spacer h={5} />
+      <Spacer h={5} />
 
-  <Grid.Container  justify="center">
-    {dirs.length !== 0 && !deleteMode &&
-      dirs.map((dir) => (
-        <Grid xs={24} md={12} lg={8} key={dir.id}>
-          <Dir dir={dir} setPath={setPath} />
-        </Grid>
-      ))}
-    {files.length !== 0 && !deleteMode &&
-      files.map((file) => (
-        <Grid xs={24} md={12} lg={8} key={file.id}>
-          <File file={file} />
-        </Grid>
-      ))}
+      <Grid.Container justify="center">
+        {dirs.length !== 0 && !deleteMode &&
+          dirs.map((dir) => (
+            <Grid xs={24} md={12} lg={8} key={dir.id}>
+              <Dir dir={dir} setPath={setPath} />
+            </Grid>
+          ))}
+        {files.length !== 0 && !deleteMode &&
+          files.map((file) => (
+            <Grid xs={24} md={12} lg={8} key={file.id}>
+              <File file={file} />
+            </Grid>
+          ))}
 
-{deleteDirs.length !== 0 && deleteMode &&
-      deleteDirs.map((dir) => (
-        <Grid xs={24} md={12} lg={8} key={dir.id}>
-        <Button type="error" iconRight={<Folder />} auto 
-        onClick={()=>handleDeleteView(dir.id, dir.title)} />
-        <Text>{dir.title}</Text>
-        </Grid>
-      ))}
-    {deleteFiles.length !== 0 && deleteMode &&
-      deleteFiles.map((file) => (
-        <Grid.Container xs={24} md={12} lg={8} key={file.id}>
-      <Button type="error" iconRight={<FileText />} auto 
-      onClick={()=>handleDeleteView(file.id, file.title)} />
-      <Text>{file.title}</Text>
-        </Grid.Container>
-      ))}
-  </Grid.Container>
-</>
+        {deleteDirs.length !== 0 && deleteMode &&
+          deleteDirs.map((dir) => (
+            <Grid xs={24} key={dir.id}>
+              <Button type="error" iconRight={<Folder />} auto
+                onClick={() => handleDeleteView(dir.id, dir.title)} />
+              <Text>{dir.title}</Text>
+            </Grid>
+          ))}
+        {deleteFiles.length !== 0 && deleteMode &&
+          deleteFiles.map((file) => (
+            <Grid.Container xs={24} key={file.id}>
+              <Button type="error" iconRight={<FileText />} auto
+                onClick={() => handleDeleteView(file.id, file.title)} />
+              <Text>{file.title}</Text>
+            </Grid.Container>
+          ))}
+      </Grid.Container>
+    </>
 
   );
 };
